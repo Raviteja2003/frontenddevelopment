4419a7a1787d5aa30ea6963975d3daea9c382490
Raviteja2003 <ravitejat0406@gmail.com>
2022-10-01 08:42:32 +0530

    React Files added

---
+++ REACT_FILES/practiceapp/src/App.js
@@ -0,0 +1,43 @@
+import './App.css';
+//import ChangeColors from "./components/ChangeColors"
+//import DisplayData from './components/DisplayData';
+//import { DigitalClock } from './components/DigitalClock';
+//import { Parent } from './components/LiftingStateUp/Parent';
+import ParentComponent from './components/ParentComponent';
+
+/*function App() {
+  return (
+    <div style={{width:"90%", margin:'0 auto'}}>
+      <ChangeColors/>
+    </div>
+  );
+}*/
+/*function App() {
+  return (
+  <div>
+  <DisplayData />
+  </div>
+  );
+}*/
+// function App() {
+//   return (
+//   <div style={{width:'90%', margin: '0 auto'}}>
+//   <DigitalClock />
+//   </div>
+//   );
+// }
+// function App() {
+//   return (
+//   <div>
+//   <Parent />
+//   </div>
+//   );
+//  }
+function App() {
+  return (
+  <div>
+  <ParentComponent />
+  </div>
+  );
+ }
+export default App;
