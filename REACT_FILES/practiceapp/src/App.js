c5014119249e289b963451019db0ea049599647a
Raviteja2003 <ravitejat0406@gmail.com>
2022-10-22 09:51:15 +0530

    ONLY blog context demo left

--- REACT_FILES/practiceapp/src/App.js
+++ REACT_FILES/practiceapp/src/App.js
@@ -3,7 +3,13 @@ import './App.css';
 //import DisplayData from './components/DisplayData';
 //import { DigitalClock } from './components/DigitalClock';
 //import { Parent } from './components/LiftingStateUp/Parent';
-import ParentComponent from './components/ParentComponent';
+//import ParentComponent from './components/ParentComponent';
+//import Hook3 from './components/Hook3';
+//import DigiClock from './DigiClock';
+//import ViewItems from './components/ViewItems';
+//import BlogContextDemo from './components/BlogContextDemo';
+//import DataFetching from './components/DataFetching';
+import Apps from './components/Apps';
 
 /*function App() {
   return (
@@ -33,11 +39,51 @@ import ParentComponent from './components/ParentComponent';
 //   </div>
 //   );
 //  }
-function App() {
+/*function App() {
   return (
-  <div>
+<div>
   <ParentComponent />
   </div>
   );
+ }*/
+ 
+ /*function app(){
+      return(
+        <div>
+          <Hook3/>
+        </div>
+      )
+ }*/
+
+ /*function app(){
+  return(
+    <div>
+      <DigiClock/>
+    </div>
+  )
+ }*/
+
+ /*function app(){
+  return(
+    <div>
+      <ViewItems/>
+    </div>
+  )
+ }*/
+
+ /*function app(){
+  return(
+    <div>
+      <DataFetching/>
+    </div>
+  )
+ }*/
+
+ function app(){
+  return(
+    <div>
+      <Apps/>
+    </div>
+  )
  }
-export default App;
+export default app;
